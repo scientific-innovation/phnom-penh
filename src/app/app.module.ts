c0927cbbd6726888dfade1f77999b8de0f960a60
Eang Senghok <senghok.eang@gmail.com>
2016-11-11 14:29:13 +0700

    Add new customer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,25 @@
-import { NgModule } from '@angular/core';
-import { IonicApp, IonicModule } from 'ionic-angular';
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import {NgModule} from "@angular/core";
+import {IonicApp, IonicModule} from "ionic-angular";
+import {MyApp} from "./app.component";
+import {HomePage} from "../pages/home/home";
+import {CustomerComponent} from "../pages/customer/customer.component";
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage
-  ],
-  imports: [
-    IonicModule.forRoot(MyApp)
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage
-  ],
-  providers: []
+    declarations: [
+        MyApp,
+        HomePage,
+        CustomerComponent
+    ],
+    imports: [
+        IonicModule.forRoot(MyApp)
+    ],
+    bootstrap: [IonicApp],
+    entryComponents: [
+        MyApp,
+        HomePage,
+        CustomerComponent
+    ],
+    providers: []
 })
-export class AppModule {}
+export class AppModule {
+}
---
+++ src/pages/customer/customer.component.ts
@@ -0,0 +1,14 @@
+import {Component} from "@angular/core";
+@Component({
+    template: `
+<ion-header>
+<ion-navbar>
+<ion-title>Test Git</ion-title>
+</ion-navbar>
+</ion-header>
+`
+})
+export class CustomerComponent {
+    constructor() {
+    }
+}
